275f759cd97b273c308c7021e62aa2c9d571fc58
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-10-20 18:48:30 +0200

    Zombie route

--- src/components/BaseNavBar.vue
+++ src/components/BaseNavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
+    <router-link to="/🧟‍♂️">Home 🧟‍♂️</router-link>
     |
     <router-link to="/about">About</router-link>
   </div>
