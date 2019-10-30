5346020bf764711773df1df4ced80d2238d43f74
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-10-30 10:13:01 +0100

    Update footer

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -2,9 +2,10 @@
   <footer class="sticky">
     <div class="content">
       <ul class="list">
-        <li>By <strong>Jorge Baumann</strong> </li>
-        <li><a href="https://github.com/baumannzone/zombie-css" target="_blank">Github</a></li>
-        <li><a href="https://twitter.com/baumannzone" target="_blank">Twitter</a></li>
+        <li>By Jorge Baumann</li>
+        <li><a class="social-gh" href="https://github.com/baumannzone/zombie-css" target="_blank">Github</a></li>
+        <li><a class="social-tw" href="https://twitter.com/baumannzone" target="_blank">Twitter</a></li>
+        <li><a class="social-yt" href="https://twitter.com/baumannzone" target="_blank">Youtube</a></li>
         <!--<li>Youtube</li>-->
       </ul>
     </div>
@@ -18,6 +19,10 @@ export default {
 </script>
 
 <style lang="stylus">
+  color-gh = #242a2e
+  color-tw = #1da1f2
+  color-yt = #d8111d
+
   footer.sticky
     position fixed
     display block
@@ -31,11 +36,31 @@ export default {
 
     ul.list
       list-style none
+
       &:before,
       &:after
         content '·'
+
       li
         display inline-block
         margin 0 10px
+
+        a
+          font-weight bold
+
+          &.social-gh
+            color color-gh
+            &:hover
+              color darken(color-gh, 10)
+
+          &.social-tw
+            color color-tw
+            &:hover
+              color darken(color-tw, 10)
+
+          &.social-yt
+            color color-yt
+            &:hover
+              color darken(color-yt, 10)
 
 </style>
